78c32456d5ab143f8830485b2d84d5a61b8833d4
Anshupadhyay1419 <anshupadhyay1406@gmail.com>
2026-09-11 21:07:55 +0530

    seeing histogram

--- binary.py
+++ binary.py
@@ -1,5 +1,8 @@
 import cv2
-
+import matplotlib.pyplot as plt
 image = cv2.imread('images/1.png' , cv2.IMREAD_GRAYSCALE)
-cv2.imshow('image', image)
-cv2.waitKey(0)
+# cv2.imshow('image', image)
+# cv2.waitKey(0)
+
+plt.hist(image.ravel(),256,[0,256])
+plt.show()
